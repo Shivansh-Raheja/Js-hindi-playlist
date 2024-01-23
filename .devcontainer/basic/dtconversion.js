a7c1ee98ca0c845a52f361a0ca377545ba65f8f7
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2024-01-23 11:02:21 +0000

    convert

---
+++ .devcontainer/basic/dtconversion.js
@@ -0,0 +1,42 @@
+/*let score=33
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum) number to number conversion*/
+
+/*let score="33"
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum) string to number conversion output number 33*/
+
+/*let score="33abc"
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum) string to number conversion output-:NaN*/
+
+/*let score=null
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum) null to number conversion output-:0*/
+
+/*let score=undefined
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum)undefined to number conversion output-:NaN*/
+
+/*let score=true
+console.log(typeof score)
+
+let scoretonum=Number(score)
+console.log(scoretonum)
+console.log(typeof scoretonum)boolean to number conversion output-:1*/
+
