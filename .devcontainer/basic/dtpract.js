7cb66b08338ad0dae4de658ac5335850f2af0f5c
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2024-01-23 10:46:52 +0000

    new file

---
+++ .devcontainer/basic/dtpract.js
@@ -0,0 +1,32 @@
+"use-strict";// to display or run every line of code as a newer version of JS
+
+//alert(3+3) cannot use this since we are using nodejs not browser
+
+console.log("don't use alert in node.js")
+console.log("Use tc39/ECMA and mdn for documentation")
+
+console.log("\n")
+
+let names="shivansh"
+let age=22
+let iscoding=true
+
+console.log("Representing vartiables with their datatypes")
+console.log("\n")
+
+console.log(names , "is a type of ",typeof(names))
+console.log(age , "is a type of ",typeof(age))
+console.log(iscoding , "is a type of ",typeof(iscoding))
+
+console.log("\n")
+console.log("we have discussed some data types like-: string,number,boolean , lets study some more....")
+console.log("\n")
+
+let check;
+let weight=null;
+console.log("check" , "is a type of ",typeof(check))
+console.log(weight, "is a type of ",typeof(weight))
+
+let checksymbol="@"
+console.log("checksymbol" , "is a type of ",typeof(checksymbol))
+
