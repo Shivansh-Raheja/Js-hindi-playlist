8e2ad61d17003a040cf2a2db85a4f65fc745c9ef
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2024-01-23 11:22:01 +0000

    new operations

---
+++ .devcontainer/basic/operations.js
@@ -0,0 +1,24 @@
+//operations in javascript
+console.log(2+2)
+console.log(2*2)
+console.log(2-2)
+console.log(2**3)
+console.log(4%3)
+console.log(6/4)
+
+let val=7
+let negval=-val
+console.log("neagative value of val")
+console.log(negval)
+
+console.log("€verything i$ working Fine")
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" +2 +3)
+console.log(1 + 4 + "3")
+
+let counter=1000
+let counterr=1000
+console.log("value after postfix-:", counter++)
+console.log("value after prefix of another counter with same input-:" ,++counterr)
