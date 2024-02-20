c7c7b33f0fc2fd6631d3a56dbda5f148d949daed
Shivansh Raheja <105767204+Shivansh-Raheja@users.noreply.github.com>
2024-02-20 08:20:57 +0000

    number and maths practice

---
+++ .devcontainer/basic/num.js
@@ -0,0 +1,17 @@
+const score=300
+console.log(score)
+
+const newnumber=new Number(23.896)
+console.log(newnumber)
+
+console.log(newnumber.toFixed(2))
+console.log(newnumber.toPrecision(3))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString())
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.9))
